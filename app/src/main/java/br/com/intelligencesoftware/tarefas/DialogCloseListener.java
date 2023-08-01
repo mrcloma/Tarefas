955e0b6b93b1bfc3dcd35d70b1ad7ff02fe0e541
marcelo <mrcloma@hotmail.com>
2023-08-01 19:16:52 -0300

    Primeiro commit do projeto.

---
+++ app/src/main/java/br/com/intelligencesoftware/tarefas/DialogCloseListener.java
@@ -0,0 +1,7 @@
+package br.com.intelligencesoftware.tarefas;
+
+import android.content.DialogInterface;
+
+public interface DialogCloseListener {
+    public void handleDialogClose(DialogInterface dialog);
+}
